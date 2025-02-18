79b08658f1c0aa003168791a7c599548777936b6
tin <roldachristine@gmail.com>
2025-02-18 13:35:06 +0800

    act three commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post.create.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { PostListComponent } from './posts/post-create/post-list/post-list.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
     FormsModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
 ],
   providers: [],
   bootstrap: [AppComponent]
