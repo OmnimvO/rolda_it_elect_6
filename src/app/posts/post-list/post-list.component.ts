6986e9eafab73353b4edaedc6fecbcf10a3d3aa3
tin <roldachristine@gmail.com>
2025-03-28 15:06:40 +0800

    activity-pre

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -3,35 +3,34 @@ import { Subscription } from "rxjs";
 import { Post } from "../post.model";
 import { PostsService } from "../posts.service";
 
-
-@Component ({
+@Component({
     selector: 'app-post-list',
     templateUrl: './post-list.component.html',
     styleUrls: ['./post-list.component.css'],
 })
 
 export class PostListComponent implements OnInit, OnDestroy {
-
     posts: Post[] = [];
     private postsSub!: Subscription;
 
-    constructor(public postsService: PostsService) {
+    constructor(public postsService: PostsService) {}
 
-    }
-
-    ngOnInit() {
+    ngOnInit(): void {
         this.postsService.getPosts();
         this.postsSub = this.postsService.getPostUpdatedListener()
-        .subscribe((posts: Post[]) => {
-            this.posts = posts;
-        })
+            .subscribe((posts: Post[]) => {
+                this.posts = posts;
+            });
     }
 
-    onDelete(postId: string) {
+    onDelete(postId: string){
         this.postsService.deletePost(postId);
     }
-    
-    ngOnDestroy() {
-        this.postsSub.unsubscribe();
+
+    ngOnDestroy(): void {
+        if (this.postsSub) {
+            this.postsSub.unsubscribe();
+        }
     }
 }
+
--- src/app/posts/post.model.ts
+++ src/app/posts/post.model.ts
@@ -1,5 +1,7 @@
 export interface Post {
-    id?: any,
+    id: string;
     title: string;
     content: string;
-}
+    imagePath: string;
+  }
+  
